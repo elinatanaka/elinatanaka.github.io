c86d66b8ba620562caf4f24b51593718c9fdb9fc
elinatanaka <elinatanakamain@Elinas-MacBook-Pro.local>
2017-01-23 02:36:47 -0800

    typo

--- sendemail.php
+++ sendemail.php
@@ -3,7 +3,7 @@ $name       = @trim(stripslashes($_POST['name']));
 $from       = @trim(stripslashes($_POST['email'])); 
 $subject    = @trim(stripslashes($_POST['subject'])); 
 $message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'tanakajpeg@gmail.com';//replace with your email
+$to   		= 'tanaka.jpeg@gmail.com';//replace with your email
 
 $headers   = array();
 $headers[] = "MIME-Version: 1.0";
